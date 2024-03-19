a361053483fec9b6dac0a48f62f321459b742c1f
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-19 15:06:02 +0700

    Created ItemsContext, removing useDataFetching hook, allowing use my multiple components

--- src/pages/ListDetail.js
+++ src/pages/ListDetail.js
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import { useNavigate, useParams } from 'react-router-dom';
-import useDataFetching from '../hooks/useDataFetching';
+// import useDataFetching from '../hooks/useDataFetching';
 import NavBar from '../components/NavBar/NavBar';
 import ListItem from '../components/ListItem/ListItem';
+import ItemsContext from '../context/ItemContext';
+
+
 
 const ListItemWrapper = styled.div`
   display: flex;
@@ -15,11 +18,7 @@ const ListItemWrapper = styled.div`
 function ListDetail() {
   let navigate = useNavigate();
   const { listId } = useParams();
-
-  const [loading, error, data] = useDataFetching(
-    'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items/',
-  );
-
+  const { loading, error, items: data } = useContext(ItemsContext);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
